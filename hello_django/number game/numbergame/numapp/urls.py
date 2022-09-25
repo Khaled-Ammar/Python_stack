2f0a1977ba8daa36b601305acfe259d1fe2f5f9c
khaled ammar <khal00o00@outlook.sa>
2022-09-25 11:05:42 +0300

    num game

---
+++ hello_django/number game/numbergame/numapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path     
+from . import views
+
+urlpatterns = [
+    path('', views.Number),
+    path('result/' , views.result),
+    path('show' , views.res),
+    path('again/' , views.PlayAgian),
+]
